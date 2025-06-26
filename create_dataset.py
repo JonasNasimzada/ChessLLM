c0d52eb80ee2ef35ba222f695edbf5725b2f7907
Jonas <st171703@stud.uni-stuttgart.de>
2025-06-26 20:14:15 +0200

    script adjust

--- create_dataset.py
+++ create_dataset.py
@@ -9,7 +9,7 @@ user_message = """Current chess position in FEN notation: {fen} {{what is the ne
 def instruction_format(sample):
     return {
         "messages": [
-            {"role": "system", "content": system_message.format(moves=sample["context"])},
+           # {"role": "system", "content": system_message.format(moves=sample["context"])},
             {"role": "user", "content": user_message.format(fen=sample["fen"])},
             {"role": "assistant", "content": sample["move"]}
         ]
@@ -20,31 +20,31 @@ if __name__ == "__main__":
     dataset = load_dataset("./trainings_data/", split="train")
     ds = dataset.sort(["game_index", "ply_index"])
 
-    contexts = []
-    current_game = None
-    prev_fens = []
-
-    for ex in ds:
-        gid = ex["game_index"]
-        fen = ex["fen"]
-
-        # if we hit a new game, reset history
-        if gid != current_game:
-            current_game = gid
-            prev_fens = []
-
-        # take up to the last 27 fens before the current move
-        if len(prev_fens) == 0:
-            contexts.append("no moves before")
-        else:
-            start = max(0, len(prev_fens) - 27)
-            contexts.append(", ".join(prev_fens[start:]))
-
-        # then record this move into history
-        prev_fens.append(fen)
-
-    # 3. Add the new column
-    ds = ds.add_column("context", contexts)
+    # contexts = []
+    # current_game = None
+    # prev_fens = []
+    #
+    # for ex in ds:
+    #     gid = ex["game_index"]
+    #     fen = ex["fen"]
+    #
+    #     # if we hit a new game, reset history
+    #     if gid != current_game:
+    #         current_game = gid
+    #         prev_fens = []
+    #
+    #     # take up to the last 27 fens before the current move
+    #     if len(prev_fens) == 0:
+    #         contexts.append("no moves before")
+    #     else:
+    #         start = max(0, len(prev_fens) - 27)
+    #         contexts.append(", ".join(prev_fens[start:]))
+    #
+    #     # then record this move into history
+    #     prev_fens.append(fen)
+    #
+    # # 3. Add the new column
+    # ds = ds.add_column("context", contexts)
 
     # Convert dataset to OAI messages
     dataset = ds.map(instruction_format, remove_columns=ds.column_names)
@@ -53,5 +53,5 @@ if __name__ == "__main__":
     dataset = dataset.train_test_split(train_size=0.8, test_size=0.2)
 
     # save datasets to disk
-    dataset["train"].to_json("./data/train/train_dataset.json", orient="records")
-    dataset["test"].to_json("./data/test/test_dataset.json", orient="records")
+    dataset["train"].to_json("./data/no_context/train_w_o_context_dataset.json", orient="records")
+    dataset["test"].to_json("./data/no_context/test_w_o_context_dataset.json", orient="records")
