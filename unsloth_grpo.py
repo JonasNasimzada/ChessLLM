f6dc1355ad3826a2c6899dd2cf3ea03f3f57dfe7
Jonas <st171703@stud.uni-stuttgart.de>
2025-07-12 19:41:23 +0200

    script adjust

--- unsloth_grpo.py
+++ unsloth_grpo.py
@@ -151,20 +151,20 @@ if __name__ == "__main__":
 
     )
 
-    # model = FastLanguageModel.get_peft_model(
-    #     model,
-    #     r=16,  # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
-    #     target_modules=["q_proj", "k_proj", "v_proj", "o_proj",
-    #                     "gate_proj", "up_proj", "down_proj", ],
-    #     lora_alpha=16,
-    #     lora_dropout=0,  # Supports any, but = 0 is optimized
-    #     bias="none",  # Supports any, but = "none" is optimized
-    #     # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
-    #     use_gradient_checkpointing="unsloth",  # True or "unsloth" for very long context
-    #     random_state=3407,
-    #     use_rslora=False,  # We support rank stabilized LoRA
-    #     loftq_config=None,  # And LoftQ
-    # )
+    model = FastLanguageModel.get_peft_model(
+        model,
+        r=16,  # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+        target_modules=["q_proj", "k_proj", "v_proj", "o_proj",
+                        "gate_proj", "up_proj", "down_proj", ],
+        lora_alpha=16,
+        lora_dropout=0,  # Supports any, but = 0 is optimized
+        bias="none",  # Supports any, but = "none" is optimized
+        # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+        use_gradient_checkpointing="unsloth",  # True or "unsloth" for very long context
+        random_state=3407,
+        use_rslora=False,  # We support rank stabilized LoRA
+        loftq_config=None,  # And LoftQ
+    )
 
     dataset = load_dataset("json", data_files="grpo_data/train_15_dataset.json", split="train")
 
