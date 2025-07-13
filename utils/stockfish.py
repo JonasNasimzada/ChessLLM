a9c361d7923e060ed7db4c94b7cc38d05571a581
Jonas <st171703@stud.uni-stuttgart.de>
2025-07-13 07:32:37 +0200

    script adjust

--- utils/stockfish.py
+++ utils/stockfish.py
@@ -9,13 +9,13 @@ class StockfishAgent:
         self.engine = SimpleEngine.popen_uci(stockfish_path)
         self.engine.configure(config)
 
-    def get_move(self, board: chess.Board, time_limit: float = 1.0):
+    def get_move(self, board: chess.Board, time_limit: float = 1.0, ponder: bool = False) -> chess.Move:
         """
         Get the best move from Stockfish for the given board state.
         :param board: Current chess board state.
         :param time_limit: Time limit for Stockfish to think (in seconds).
         :return: Best move as a chess.Move object.
         """
-        result = self.engine.play(board, Limit(time=time_limit))
+        result = self.engine.play(board, Limit(time=time_limit), ponder=ponder)
         print(result)
         return result.move
