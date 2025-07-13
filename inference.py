a81181f233c7ea28c93f3e35b9bcda03c7a91c9d
Jonas <st171703@stud.uni-stuttgart.de>
2025-07-13 08:53:21 +0200

    script adjust

--- inference.py
+++ inference.py
@@ -25,6 +25,8 @@ user_message_no_context = """Current position (FEN):\n{current_move}\n\nWhat is 
 
 
 def stockfish_make_move(current_board):
+    stockfish_agent = Stockfish("../stockfish-ubuntu-x86-64-avx2",
+                                parameters={"Skill Level": 0, "Debug Log File": "./stockfish_debug.log", "Hash": 2048})
     fen = current_board.fen()
     stockfish_agent.set_fen_position(fen)
     move = stockfish_agent.get_best_move()
@@ -121,8 +123,7 @@ if __name__ == "__main__":
         tokenizer,
         chat_template="llama-3.1"
     )
-    stockfish_agent = Stockfish("../stockfish-ubuntu-x86-64-avx2",
-                                parameters={"Skill Level": 0, "Debug Log File": "./stockfish_debug.log"})
+
 
     # stockfish_agent = StockfishAgent(stockfish_path="../stockfish-ubuntu-x86-64-avx2", config={"Skill Level": 0})
     play_chess()
